999fe506f0f083f6bcb99507fd96fe1000927082
Ramon Velarde <velardeglaser@Ramons-Air.home>
2022-07-14 00:49:33 -0400

    changes from 7-14

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse
+from .forms import FeedingForm
+from .models import Cat, Toy
+
 # from django.http import HttpResponse
 
 # Demo Cat Data - Replace with Database
@@ -40,10 +44,38 @@ def cats_index(request):
 
 
 def cats_detail(request, cat_id):
-    # Get the the individual cat
     cat = Cat.objects.get(id=cat_id)
-    # render template, pass it the cat
-    return render(request, 'cats/detail.html', {'cat': cat})
+    # Get the toys the cat doesn't have
+    toys_cat_doesnt_have = Toy.objects.exclude(
+        id__in=cat.toys.all().values_list('id'))
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat, 'feeding_form': feeding_form,
+        # Add the toys to be displayed
+        'toys': toys_cat_doesnt_have
+    })
+
+
+def add_feeding(request, cat_id):
+    # create the ModelForm using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('detail', cat_id=cat_id)
+
+def assoc_toy(request, cat_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Cat.objects.get(id=cat_id).toys.add(toy_id)
+  return redirect('detail', cat_id=cat_id)
+
+def rm_cat_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.remove(toy_id)
+    return redirect('detail', cat_id=cat_id)
 
 
 class CatCreate(CreateView):
@@ -51,11 +83,36 @@ class CatCreate(CreateView):
     fields = '__all__'
     success_url = '/cats/'
 
+
 class CatUpdate(UpdateView):
-  model = Cat
-  # Let's disallow the renaming of a cat by excluding the name field!
-  fields = ['breed', 'description', 'age']
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
 
 class CatDelete(DeleteView):
-  model = Cat
-  success_url = '/cats/'
+    model = Cat
+    success_url = '/cats/'
+
+
+class ToyList(ListView):
+    model = Toy
+
+
+class ToyDetail(DetailView):
+    model = Toy
+
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
